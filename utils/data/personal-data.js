41c9789e579b53bbc54e7278196806a7e6716589
ashok205050 <ashokmaurya2050@gmail.com>
2025-10-11 11:15:50 +0530

    s

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,8 +1,8 @@
 export const personalData = {
   name: "Ashok Maurya",
   profile: '/profile.png',
-  designation: "Web Developer",
-  description: "My name is Ashok Maurya. I am a and enthusiastic Web Developer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  designation: "Fullstack Web Developer",
+  description: "My name is Ashok Maurya. I'm an enthusiastic Web Developer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'ashokmaurya2050@gmail.com',
   phone: '+91-7039824106',
   address: 'Sonia vihar, Delhi - 110090',
